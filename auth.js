bc54a9653792546bbd851e38309f01302122b6dc
Antonio Neto <antoniofqn@hotmail.com>
2023-06-26 20:51:22 -0300

    logs

--- auth.js
+++ auth.js
@@ -2,7 +2,6 @@ import jwt from 'jsonwebtoken';
 
 // Middleware to verify the JWT token
 export function authenticateToken(req, res, next) {
-  // Get the token from the request headers, query parameters, or cookies
   const token = req.headers.authorization?.split(' ')[1] || req.query.token || req.cookies.token;
 
   if (!token) {
@@ -10,16 +9,14 @@ export function authenticateToken(req, res, next) {
   }
 
   try {
-    // Verify and decode the token
     const decoded = jwt.verify(token, "flamengo");
-    req.user = decoded; // Attach the decoded payload to the request object
+    req.user = decoded;
     next();
   } catch (error) {
     return res.status(403).json({ error: 'Invalid token' });
   }
 }
 
-// Generate a JWT token with a payload
 export function generateToken(payload) {
   const token = jwt.sign(payload, "flamengo", { expiresIn: '1h' });
   return token;
